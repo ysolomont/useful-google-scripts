723b5249c9bd2e1103e71f02975a2ec030221db1
Yehuda Solomont <yehuda.solomont@vinciworks.com>
2017-02-13 10:39:00 +0200

    Added find duplicates scripts

---
+++ useful-scripts.js
@@ -0,0 +1,42 @@
+function removeFullRowDuplicates() {
+  // This function compares a full row and removes exact duplicates
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var data = sheet.getDataRange().getValues();
+  var newData = new Array();
+  for(i in data){
+    var row = data[i];
+    var duplicate = false;
+    for(j in newData){
+      if(row.join() == newData[j].join()){
+        duplicate = true;
+      }
+    }
+    if(!duplicate){
+      newData.push(row);
+    }
+  }
+  sheet.clearContents();
+  sheet.getRange(1, 1, newData.length, newData[0].length).setValues(newData);
+}
+
+function removeSemiDuplicates() {
+  // This finds duplicates in the first two columns
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var data = sheet.getDataRange().getValues();
+  var newData = new Array();
+  for(i in data){
+    var row = data[i];
+    var duplicate = false;
+    for(j in newData){
+      // This conditional statement will find duplicates each time two rows have the same data in the first and the second column of the sheet.
+      if(row[0] == newData[j][0] && row[1] == newData[j][1]){
+          duplicate = true;
+      }
+    }
+    if(!duplicate){
+      newData.push(row);
+    }
+  }
+  sheet.clearContents();
+  sheet.getRange(1, 1, newData.length, newData[0].length).setValues(newData);
+}
